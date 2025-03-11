60b22461fdf6c5c7251a1eb9820bfc46f2f3ce1d
oops <asiyanoushad1515@gmail.com>
2025-03-11 15:05:10 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,22 @@
+public class string{
+
+}
+public static void main(String[] args){
+/*String s1="asiya";
+System.out.println(" lower case :"+ s1.toLowerCase());*/
+
+
+/*String s1="helllo";
+String s2="world";
+String s3=s1.concat (s2);
+System.out.println(s3);*/
+
+/*String s1="asiya Noushad";
+System.out.println(" length of the string is "+s1.length());
+System.out.println("the character at second and fourth position is"+s1.charAt(1)+ " , " +s1.charAt(3));
+System.out.println("the substring of the string is "+ s1.substring (6, 13));
+System.out.println("the sub string using start index is " + s1.substring(0,6 ));*/
+
+
+
+}
