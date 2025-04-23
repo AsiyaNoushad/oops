daa276046330fcb5f9478d49b056989cbea2393a
oops <asiyanoushad1515@gmail.com>
2025-04-23 11:59:04 +0530

    Add files via upload

---
+++ StackDemo.java
@@ -0,0 +1,32 @@
+import java.util.*; 
+import java.io.*; 
+
+public class StackDemo { 
+    public static void main(String args[]) { 
+        // Creating an empty Stack 
+        Stack<Integer> stk = new Stack<Integer>(); 
+        
+        // Use push() method to add elements to the stack
+        stk.push(10); 
+        stk.push(15); 
+        stk.push(30); 
+        stk.push(20); 
+        stk.push(5); 
+        
+        // Displaying the Stack 
+        System.out.println("Initial Stack: " + stk); 
+        
+        // Fetching the element at the top of the Stack
+        System.out.println("The element at the top of the stack is: " + stk.peek()); 
+        
+        // Removing elements using pop() method
+        System.out.println("Popped element: " + stk.pop()); 
+        System.out.println("Popped element: " + stk.pop()); 
+        
+        // Displaying the Stack after pop operation 
+        System.out.println("Stack after pop operation: " + stk); 
+        
+        // Fetching the element at the top of the Stack after popping
+        System.out.println("The element at the top of the stack is: " + stk.peek()); 
+    } 
+}
